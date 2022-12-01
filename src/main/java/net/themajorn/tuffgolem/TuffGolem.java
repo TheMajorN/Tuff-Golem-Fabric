925d01ff8b007795dfd4f5626665f662c3810372
TheMajorN <nickbaranosky@gmail.com>
2022-12-01 17:08:39 -0500

    Initial commit

---
+++ src/main/java/net/themajorn/tuffgolem/TuffGolem.java
@@ -0,0 +1,15 @@
+package net.themajorn.tuffgolem;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TuffGolem implements ModInitializer {
+	public static final String MOD_ID = "tuffgolem";
+	public static final Logger LOGGER = LoggerFactory.getLogger("tuffgolem");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/themajorn/tuffgolem/TuffGolemClient.java
@@ -0,0 +1,10 @@
+package net.themajorn.tuffgolem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TuffGolemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
